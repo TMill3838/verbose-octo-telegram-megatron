d205a1129ae9eebd6968dff5f3fb40d10d49130c
TMill3838 <rabbitgolden101@gmail.com>
2026-02-18 18:53:22 -0600

    Add files via upload

---
+++ Searchlab/data/data.js
@@ -0,0 +1,18 @@
+const jsObjectData = [
+    /* Autobot */
+    { id: 1, name: "Optimus Prime", faction: "Autobot" },
+    { id: 2, name: "Bumblebee", faction: "Autobot" },
+    { id: 3, name: "Grimlock", faction: "Autobot" },
+    { id: 4, name: "Jazz", faction: "Autobot" },
+    { id: 5, name: "Springer", faction: "Autobot" },
+    { id: 6, name: "Blaster", faction: "Autobot" },
+
+
+    /*Decepticon */
+    { id: 1, name: "Megatron", faction: "Decepticon" },
+    { id: 2, name: "Starscream", faction: "Decepticon" },
+    { id: 3, name: "Shockwave", faction: "Decepticon" },
+    { id: 4, name: "Knockout", faction: "Decepticon" },
+    { id: 5, name: "Blitzwing", faction: "Decepticon" },
+    { id: 6, name: "Soundwave", faction: "Decepticon" },
+];
